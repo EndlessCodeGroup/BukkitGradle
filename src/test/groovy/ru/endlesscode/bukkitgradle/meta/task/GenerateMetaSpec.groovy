7b5e3c3f016b74d230a94a8bf937c04b64011920
Osip Fatkullin <osip.fatkullin@gmail.com>
2020-05-17 10:33:50 +0300

    test: Fix GenerateMetaSpec

--- src/test/groovy/ru/endlesscode/bukkitgradle/meta/task/GenerateMetaSpec.groovy
+++ src/test/groovy/ru/endlesscode/bukkitgradle/meta/task/GenerateMetaSpec.groovy
@@ -1,11 +1,15 @@
 package ru.endlesscode.bukkitgradle.meta.task
 
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
 import ru.endlesscode.bukkitgradle.PluginSpecification
 import ru.endlesscode.bukkitgradle.meta.MetaFile
 
 class GenerateMetaSpec extends PluginSpecification {
 
-    File metaFile
+    private final static TASK_PATH = ':generatePluginMeta'
+
+    private File metaFile
 
     def setup() {
         metaFile = file("build/tmp/generatePluginMeta/$MetaFile.NAME")
@@ -13,13 +17,20 @@ class GenerateMetaSpec extends PluginSpecification {
 
     def 'generate default plugin meta'() {
         when:
-        runner.withArguments('generatePluginMeta').build()
+        def result = generatePluginMeta()
 
         then:
-        metaFile.text == """
+        result.task(TASK_PATH).outcome == TaskOutcome.SUCCESS
+
+        and:
+        metaFile.text == """\
             name: test-plugin
             main: com.example.testplugin.TestPlugin
             version: 1.0
         """.stripIndent()
+    }
+
+    private BuildResult generatePluginMeta() {
+        return runner.withArguments(TASK_PATH).build()
     }
 }
