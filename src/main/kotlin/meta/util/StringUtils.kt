e61d811725b68507d101fcbf18467dbae7b5b950
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-01-01 19:48:02 +0300

    refactor: Remove StringUtils.resolveString

--- src/main/kotlin/meta/util/StringUtils.kt
+++ src/main/kotlin/meta/util/StringUtils.kt
@@ -1,20 +1,6 @@
 package ru.endlesscode.bukkitgradle.meta.util
 
-import groovy.lang.Closure
-
 internal object StringUtils {
-
-    /** Resolves String from the given [obj]. */
-    @JvmStatic
-    fun resolve(obj: Any?): String? {
-        return when (obj) {
-            null -> null
-            is String -> obj
-            is Closure<*> -> resolve(obj.call())
-            is Class<*> -> obj.name
-            else -> obj.toString()
-        }
-    }
 
     /** Converts [text] to PascalCase. */
     @JvmStatic
--- src/test/groovy/ru/endlesscode/bukkitgradle/meta/util/StringUtilsSpec.groovy
+++ src/test/groovy/ru/endlesscode/bukkitgradle/meta/util/StringUtilsSpec.groovy
@@ -4,22 +4,6 @@ import spock.lang.Specification
 
 class StringUtilsSpec extends Specification {
 
-    void 'when resolve value - should return expected result'() {
-        expect:
-        expectedResult == StringUtils.resolve(value)
-
-        where:
-        expectedResult                                      | value
-        null                                                | null
-        null                                                | { null }
-        ""                                                  | ""
-        "Some text"                                         | "Some text"
-        "42"                                                | 42
-        "42"                                                | { 42 }
-        "I'm lazy"                                          | { "I'm lazy" }
-        "ru.endlesscode.bukkitgradle.meta.util.StringUtils" | StringUtils
-    }
-
     void 'when toPascalCase - should return camel cased value'() {
         expect:
         StringUtils.toPascalCase(value) == expectedResult
