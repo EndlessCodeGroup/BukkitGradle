50f82ae69d0e2f595b19ba72a2c7575c3e8398ed
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-01-04 17:27:11 +0300

    kotlin: Move PrepareServer to Kotlin

--- src/main/groovy/ru/endlesscode/bukkitgradle/server/legacy/LegacyDevServerPlugin.groovy
+++ src/main/groovy/ru/endlesscode/bukkitgradle/server/legacy/LegacyDevServerPlugin.groovy
@@ -2,13 +2,15 @@ package ru.endlesscode.bukkitgradle.server.legacy
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.TaskProvider
-import ru.endlesscode.bukkitgradle.BukkitGradlePlugin
+import org.gradle.jvm.tasks.Jar
+import ru.endlesscode.bukkitgradle.meta.extension.PluginMeta
 import ru.endlesscode.bukkitgradle.server.ServerConstants
 import ru.endlesscode.bukkitgradle.server.extension.RunConfiguration
-import ru.endlesscode.bukkitgradle.server.legacy.task.PrepareServer
 import ru.endlesscode.bukkitgradle.server.task.CreateIdeaJarRunConfiguration
 import ru.endlesscode.bukkitgradle.server.task.GenerateRunningScript
+import ru.endlesscode.bukkitgradle.server.task.PrepareServer
 import ru.endlesscode.bukkitgradle.server.task.RunServer
 
 class LegacyDevServerPlugin implements Plugin<Project> {
@@ -23,13 +25,7 @@ class LegacyDevServerPlugin implements Plugin<Project> {
             scriptDir.set(serverCore.serverDir)
         }
 
-        def prepareServer = project.tasks.register('prepareServer', PrepareServer) {
-            group = BukkitGradlePlugin.GROUP
-            description = 'Prepare server ro run. Configure server and copy compiled plugin to plugins dir'
-            core = serverCore
-            dependsOn('build', 'copyServerCore')
-        } as TaskProvider<PrepareServer>
-
+        def prepareServer = configurePrepareServerTask(project, serverCore)
         project.tasks.register('runServer', RunServer) {
             scriptFile.set(generateRunningScript.map { it.scriptFile.get().asFile })
             dependsOn(prepareServer)
@@ -39,7 +35,26 @@ class LegacyDevServerPlugin implements Plugin<Project> {
             configurationName.set("$project.name: Run server")
             beforeRunTask.set('prepareServer')
             configurationsDir.set(project.rootProject.layout.projectDirectory.dir('.idea/runConfigurations'))
-            jarPath.set(prepareServer.map { it.serverDir.file(ServerConstants.FILE_CORE).get().asFile })
+            jarPath.set(new File(serverCore.serverDir, ServerConstants.FILE_CORE))
+        }
+    }
+
+    private static TaskProvider<PrepareServer> configurePrepareServerTask(Project project, ServerCore serverCore) {
+        def jarTaskName = project.plugins.hasPlugin("com.github.johnrengelman.shadow") ? "shadowJar" : "jar"
+        def jarTask = project.tasks.named(jarTaskName, Jar) as TaskProvider<Jar>
+        def meta = project.bukkit.meta as PluginMeta
+        def copyPlugins = project.tasks.register("copyPlugins", Copy) {
+            from(jarTask)
+            into(project.mkdir(new File(serverCore.serverDir, "plugins")))
+            rename { "${meta.name.get()}.jar" }
+        }
+
+        def run = project.bukkit.run as RunConfiguration
+        return project.tasks.register('prepareServer', PrepareServer) {
+            serverDir.set(serverCore.serverDir)
+            eula = run.eula
+            onlineMode = run.onlineMode
+            dependsOn('copyServerCore', copyPlugins)
         }
     }
 }
--- src/main/kotlin/Bukkit.kt
+++ src/main/kotlin/Bukkit.kt
@@ -3,9 +3,11 @@ package ru.endlesscode.bukkitgradle
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
 import ru.endlesscode.bukkitgradle.meta.extension.PluginMeta
+import ru.endlesscode.bukkitgradle.server.extension.RunConfiguration
 
 public interface Bukkit {
     public val meta: PluginMeta
+    public val run: RunConfiguration
 }
 
 internal val Project.bukkit: Bukkit get() = extensions.getByType()
