5bd28af1852311146a981c39110dfa83bf4fdacb
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-01-16 10:36:48 +0300

    update: Gradle 6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     `kotlin-dsl`
     `maven-publish`
     groovy
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.serialization") version "1.4.21"
+    kotlin("plugin.serialization") version "1.4.20"
     id("com.gradle.plugin-publish") version "0.12.0"
     id("com.github.ben-manes.versions") version "0.36.0"
 }
